f8b8b5f3aa646a2b564ebf0ea7f21e4d4ee386a3
unknown <vamsienduri1234@gmail.com>
2026-09-07 11:52:56 +0530

    async/await and fetch() apicalling

---
+++ js/async/day2.js
@@ -0,0 +1,286 @@
+// console.log("vamsi")
+// console.log("vamsi")
+// console.log("vamsi")
+
+// let a=10
+// let b=20
+
+// function login(){
+// //
+// }
+// login()
+
+// setTimeout(()=>{
+// console.log("settimeout with 2 s")
+// },2000)
+
+// setTimeout(()=>{
+// console.log("settimeout with 0 s")
+// },0)
+
+// setTimeout(()=>{
+// console.log("settimeout with 4.5 s")
+// },4500)
+
+// setInterval(()=>{
+//  console.log("interval with 5 s")
+// },5000)
+
+// console.log("vamsiEnduri")
+// console.log("vamsiEnduri")
+// console.log("vamsiEnduri")
+
+// function a() {
+//   function b() {
+//     function c() {
+//       function d() {
+//         function e() {
+//           function f() {
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             console.log("vamsi");
+//             function g() {
+//               function h() {}
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// }
+
+// code debugging will be very tough
+// code maintaince is very tough
+
+// setTimeout(() => {
+//   console.log("upper setTimeout");
+// //   setTimeout(() => {
+//     console.log("inner setTimout");
+//     setTimeout(() => {
+//       console.log("inne-rmost setTimout with 5 s");
+//       setTimeout(()=>{
+//         setTimeout(()=>{
+//             setTimeout(()=>{
+//                 setTimeout(()=>{
+//                     setTimeout(()=>{
+
+//                     })
+//                 })
+//             })
+//         })
+//       })
+//     }, 5000);
+//   }, 0);
+// }, 2000);
+
+
+
+
+
+// setTimeout(()=>{
+//     console.log("registered")
+//     setTimeout(()=>{
+//         console.log("login")
+//         setTimeout(()=>{
+//             console.log("role")
+//             setTimeout(()=>{
+//                 console.log("dashboard")
+//                 setTimeout(()=>{
+//                     console.log("products")
+//                     setTimeout(()=>{
+//                         console.log("addTocart")
+//                         console.log("asdfghjk") // error 
+//                         setTimeout(()=>{
+//                             console.log("proceed to payment")
+//                             setTimeout(()=>{
+//                                 console.log("payemnet pending")
+//                                 setTimeout(()=>{
+//                                     console.log("navigating to ph pay")
+//                                     setTimeout(()=>{
+//                                         console.log("payemnt done ")
+//                                         setTimeout(()=>{
+//                                             console.log("redicrecting to dashboard")
+//                                         })
+//                                     })
+//                                 })
+//                             })
+//                         })
+                        
+//                     })
+//                 })
+//             })
+//         })
+//     })
+// })
+
+
+// what is promise ""
+// promise is a js object which initailly inloves in pending stage and have 2 more stages like successful and rejected 
+
+// syntax :-- 
+
+let pObj=new Promise((resolve,reject)=>{
+    // code 
+    if (true){
+        resolve("data retreiving is successfull")
+        console.log("successful")
+        // api calling  :-- https://fakest23456oreapi.com/productsssss
+    }else{
+        reject("rejected")
+    }
+})
+
+// console.log(typeof(pObj))
+pObj.then(()=>{
+    console.log("data displaying")
+}).then(()=>{
+  console.log("browsing the products")
+}).then(()=>{
+    console.log("add to cart")
+}).then(()=>{
+    console.log("proceed to payment")
+})
+.catch(()=>{
+    console.log("something went wrong")
+})
+// console.log(pObj)
+
+// Promise={
+//     then:function then(){
+
+//     },
+//     catch:function catch(){
+
+//     }
+// }
+
+// Promise.then()
+// Promise.catch()
