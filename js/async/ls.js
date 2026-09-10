146cc92c0b1a3cc4363a81f26acc97ad0708e973
unknown <vamsienduri1234@gmail.com>
2026-09-10 11:16:04 +0530

    ls refresh + btn click + dlet op completed

--- js/async/ls.js
+++ js/async/ls.js
@@ -1,57 +1,133 @@
-let btn_values=["Get","Post","Delete","Update"]
-let btns_c=document.getElementById("btns")
-let t_body=document.getElementById("tbody")
-btn_values.map((elem,index)=>{
-    let btn=document.createElement("button")
-    btn.textContent=elem
-    let all_users=JSON.parse(localStorage.getItem("all_users"))
-    btn.onclick=()=>{
-        let btn_type=elem
+let btn_values = ["Get", "Post"];
+let btns_c = document.getElementById("btns");
+let t_body = document.getElementById("tbody");
+let all_users = JSON.parse(localStorage.getItem("all_users"));
 
-        if (btn_type == "Get"){
-            alert("get op")
-            all_users.map((elem,index)=>{
-                let tr=document.createElement("tr")
+btn_values.map((elem, index) => {
+  let btn = document.createElement("button");
+  btn.textContent = elem;
+  
+  // btn.onclick=()=>{
+  //     let btn_type=elem
 
-                tr.innerHTML=`
-                <td>${index+1}</td>
+  //     if (btn_type == "Get"){
+  //         alert("get op")
+  //         all_users.map((elem,index)=>{
+  //             let tr=document.createElement("tr")
+
+  //             tr.innerHTML=`
+  //             <td>${index+1}</td>
+  //             <td>${elem.name}</td>
+  //             <td>${elem.email}</td>
+  //             <td><button>Delete</button></td>
+  //             <td><button>Update</button></td>
+  //             `
+  //             t_body.append(tr)
+  //         })
+
+  //     }
+  //     else if (btn_type == "Post"){
+  //         alert("post op")
+  //         n=prompt("enter your name")
+  //         e=prompt("enter your email")
+  //         p=prompt("enter your password")
+  //         c_p=prompt("enter your confirm-password")
+
+  //         if (p == c_p){
+
+  //             let new_user={
+  //                 name:n,
+  //                 email:e,
+  //                 password:p
+  //             }
+  //             all_users.push(new_user)
+  //             localStorage.setItem("all_users",JSON.stringify(all_users))
+  //         }
+  //     }
+  //     else if (btn_type == "Delete"){
+  //         alert("delete op")
+  //     }
+  //     else if (btn_type == "Update"){
+  //         alert("update op")
+  //     }
+  // }
+  btn.addEventListener("click", () => {
+    let btn_type=elem
+    if (elem == "Get") {
+      displyData(all_users);
+    } else if (btn_type == "Post") {
+      alert("post op");
+      n = prompt("enter your name");
+      e = prompt("enter your email");
+      p = prompt("enter your password");
+      c_p = prompt("enter your confirm-password");
+
+      if (p == c_p) {
+        let new_user = {
+          name: n,
+          email: e,
+          password: p,
+        };
+        all_users.push(new_user); 
+        localStorage.setItem("all_users", JSON.stringify(all_users));
+        displyData(all_users)
+      }
+    } else if (btn_type == "Delete") {
+      alert("delete op");
+
+    } else if (btn_type == "Update") {
+      alert("update op");
+    }
+  });
+  btn.classList.add("api-btn");
+  btn.classList.add(elem.toLowerCase());
+
+  btns_c.append(btn);
+});
+
+
+
+function displyData(all_users) {
+
+    t_body.innerHTML=""
+
+    all_users.map((elem, index) => {
+      let tr = document.createElement("tr");
+
+      tr.innerHTML = `
+                <td>${index + 1}</td>
                 <td>${elem.name}</td>
                 <td>${elem.email}</td>
-                <td><button>Delete</button></td>
+                <td><button class="dlt_bn">Delete</button></td>
                 <td><button>Update</button></td>
-                `
-                t_body.append(tr)
-            })
-            
-        }
-        else if (btn_type == "Post"){
-            alert("post op")
-            n=prompt("enter your name")
-            e=prompt("enter your email")
-            p=prompt("enter your password")
-            c_p=prompt("enter your confirm-password")
+                `;
 
-            if (p == c_p){
-                
-                let new_user={
-                    name:n,
-                    email:e,
-                    password:p
-                }
-                all_users.push(new_user)  
-                localStorage.setItem("all_users",JSON.stringify(all_users))
-            }
-        }
-        else if (btn_type == "Delete"){
-            alert("delete op")
-        }
-        else if (btn_type == "Update"){
-            alert("update op")
-        }
+                let btn_delete=tr.querySelector(".dlt_bn")
+                btn_delete.addEventListener("click",()=>{
+                    deleteUser(elem.email)
+                })
+      t_body.append(tr);
+    });
+  
+}
+
+
+function deleteUser(incomingEmail){
+    let status_d=confirm("are you sure to dlete ?")
+    if(status_d){
+        all_users=all_users.filter((elem,index)=>{
+        return elem.email !== incomingEmail
+    })
+
+    localStorage.setItem("all_users",JSON.stringify(all_users))
+    displyData(all_users)
+    }else{
+        alert("delete cancelled")
     }
-    btn.classList.add("api-btn");
-    btn.classList.add(elem.toLowerCase());
+}
 
-    btns_c.append(btn)
+
+document.addEventListener("DOMContentLoaded",()=>{
+    displyData(all_users)
 })
-
+// displyData()
