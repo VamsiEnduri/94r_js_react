a0f29e3f81518030f3ed466d165ceb04f2c67870
unknown <vamsienduri1234@gmail.com>
2026-09-11 10:44:13 +0530

    ls with updated closed

--- js/async/ls.js
+++ js/async/ls.js
@@ -2,6 +2,7 @@ let btn_values = ["Get", "Post"];
 let btns_c = document.getElementById("btns");
 let t_body = document.getElementById("tbody");
 let all_users = JSON.parse(localStorage.getItem("all_users"));
+let update_form=document.getElementById("update_form")
 
 btn_values.map((elem, index) => {
   let btn = document.createElement("button");
@@ -99,18 +100,52 @@ function displyData(all_users) {
                 <td>${elem.name}</td>
                 <td>${elem.email}</td>
                 <td><button class="dlt_bn">Delete</button></td>
-                <td><button>Update</button></td>
+                <td><button class="upd-btn">Update</button></td>
                 `;
 
                 let btn_delete=tr.querySelector(".dlt_bn")
                 btn_delete.addEventListener("click",()=>{
                     deleteUser(elem.email)
                 })
+
+                let updte_btn=tr.querySelector(".upd-btn")
+                updte_btn.addEventListener("click",()=>{
+                  updateUser(elem,index)
+                })
+
+                
       t_body.append(tr);
     });
   
 }
 
+
+function updateUser(incomingUserObj,inocmingIndexNum){
+  update_form.style.display="flex"
+  update_form.style.flexDirection="column"
+  let cnfm_udt=document.getElementById("cnfm_udt")
+  document.getElementById("name").value=incomingUserObj.name
+  document.getElementById("email").value=incomingUserObj.email
+  document.getElementById("password").value=incomingUserObj.password
+  cnfm_udt.addEventListener("click",()=>{
+    let name_u=document.getElementById("name").value
+    let email_u=document.getElementById("email").value
+    let password_u=document.getElementById("password").value
+    let updated_user={
+      name:name_u,
+      email:email_u,
+      password:password_u
+    }
+    all_users[inocmingIndexNum]=updated_user
+    localStorage.setItem("all_users",JSON.stringify(all_users))
+    displyData(all_users)
+  })
+  // update_form.style.width="100px"
+
+  // update_form.style.height="100px"
+  // update_form.style.backgroundColor="red"
+
+}
 
 function deleteUser(incomingEmail){
     let status_d=confirm("are you sure to dlete ?")
